27cf4fbbed7d7c9f3418cb433f2400cbb6dead8b
Charroud Lilian <lilian.charroud@univ-lyon2.fr>
2021-06-11 11:29:17 +0200

    aleatoire DU graphiQUE

---
+++ graphique.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <html>
+ordinateur, fais le graphique aleatoire stp
+  <body>
+    <div id="chart_div" style="width: 900px; height: 500px;"></div>
+  </body>
+</html>
+  </body>
+</html>
