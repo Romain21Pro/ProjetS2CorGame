a28062a943bab0c3efbb6d2af526557efbbbd34e
cyril becu <cyril.becu@univ_lyon2.fr>
2021-06-28 09:04:59 +0200

    avancement interface + systeme point marche

--- HTML_PHP/menu du jeu page 2.php
+++ HTML_PHP/menu du jeu page 2.php
@@ -6,22 +6,33 @@
     <title></title>
     <link rel="stylesheet" type="text/css" href="..\CSS\style menu jeu.css"/>
   </head>
+  <?php
+    session_start();
+
+    if (isset($_POST['pseudo'])) {
+      $_SESSION['pseudo']=$_POST['pseudo'];
+    }
+   ?>
   <body>
     <?php
+    if (!isset($_SESSION['pointcoeur'])) {
+    $_SESSION['pointcoeur'] = 3;
+    $_SESSION['pointargent'] = 0;
+  }
   $valeurréel = 0.75;
   $coeur = "..\Images\coeur.png";
-  $pointcoeur = 3;
-  $pointargent = 0;
+
    ?>
       <div class="gridLeft"></div>
       <div class="gridMiddle">
     <table class="center">
 <tr>
 <td>
-<?php if (abs($_POST['estimation']-$valeurréel)>0.10) {$pointcoeur=$pointcoeur-1;
-};if (abs($_POST['estimation']-$valeurréel)<0.05 && $pointcoeur<3 ) {$pointcoeur=$pointcoeur+1;$pointargent=$pointargent+5;
-};if (abs($_POST['estimation']-$valeurréel)<=0.10 && abs($_POST['estimation']-$valeurréel)>=0.05 ) {$pointargent=$pointargent+1;
-};echo "<img src=$coeur height=20>"; echo "$pointcoeur";
+
+<?php if (abs($_POST['estimation']-$valeurréel)>0.10) {$_SESSION['pointcoeur']=$_SESSION['pointcoeur']-1;
+};if (abs($_POST['estimation']-$valeurréel)<0.05 && $_SESSION['pointcoeur']<3 ) {$_SESSION['pointcoeur']=$_SESSION['pointcoeur']+1;$_SESSION['pointargent']=$_SESSION['pointargent']+5;
+};if (abs($_POST['estimation']-$valeurréel)<=0.10 && abs($_POST['estimation']-$valeurréel)>=0.05 ) {$_SESSION['pointargent']=$_SESSION['pointargent']+1;
+};echo "<img src=$coeur height=20>"; echo $_SESSION['pointcoeur'];
  ?></td>
 <td></td>
 <td><div id="high_score">high score</div>
@@ -33,21 +44,31 @@
 <tr>
   <td>  <img height="20" src="..\Images\piece.png" alt="piece"><br>
   </td>
-  <td><div><?php echo "$pointargent"; ?></div></td>
+  <td><div><?php echo $_SESSION['pointargent']; ?></div></td>
   <td> <div>0</div> </td>
   <td></td>
 </tr>
 <tr>
-  <td colspan="2"><img src="..\Images\exemple corrélation.png" alt="correlation"></td>
+  <td colspan="2">
+    <div id="r-output" id="width: 100%; padding: 25px;">
+    <?php
+      // Execute the R script within PHP code
+      // Generates output as test.png image.
+      exec('test_corr.R', $output);
+      $corr = substr($output[0], 3, 6);
+      echo $corr;
+    ?>
+    <img src="test.png?var1.1" alt="R Graph"/>
+    </div>
+  </td>
   <td></td>
     <td></td>
 </tr>
 <tr>
   <td colspan="2">
     <form  action="menu du jeu.php" method="post">
-      <input type="text" name="coeur" value=$pointcoeur>
-      <input type="text" name="argent" value=$pointargent>
-    <input id="guess"type="submit" name="suivant" value="NEXT"></td>
+    <input id="guess"type="submit" name="suivant" value="NEXT">
+  </form></td>
     <td></td>
     <td></td>
 </tr>
